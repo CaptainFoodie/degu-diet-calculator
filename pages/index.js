774e6f72c019e1cfa2eb6ba0d57305e5b7712930
CaptainFoodie <andrej.alfonso@gmail.com>
2025-06-19 18:15:44 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Osmák Dietní Kalkulačka</title>
+        <title>Osmák Dietní Kalkulačka v2</title>
       </Head>
       <main style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-        <h1 style={{ fontSize: '24px', fontWeight: 'bold' }}>Kalkulačka denního krmení pro osmáky</h1>
+        <h1 style={{ fontSize: '24px', fontWeight: 'bold' }}>Kalkulačka denního krmení pro osmáky – verze 2</h1>
         <OsmakDietCalculator />
       </main>
     </div>
